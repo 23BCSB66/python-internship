85503b9c7b964aa3e78eb16f52423992531c58d8
Asish Kumar Behera <beheraasish2004@gmail.com>
2024-07-12 13:45:27 +0530

    Day 15 Q1

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(my, name, age, gender):
+        my.name = name
+        my.age = age
+        my.gender = gender
+
+    def introduce(my):
+        print(f"Hello, my name is {my.name}. I am {my.age} years old and I am {my.gender}.")
+
+
+person = Person("Asish", 20, "Male")
+person.introduce()
